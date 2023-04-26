130bf92b4b7f6c4038de794bf0e2e2c0f259edeb
stevegustason <steve.gustason@gmail.com>
2023-04-26 12:55:35 -0500

    Initial Commit

---
+++ CoreDataProject/CoreDataProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataProjectApp.swift
+//  CoreDataProject
+//
+//  Created by Steven Gustason on 4/26/23.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
