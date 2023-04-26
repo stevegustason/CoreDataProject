433ddeab415ef38c62213738565ba1a2580da26d
stevegustason <steve.gustason@gmail.com>
2023-04-26 14:53:35 -0500

    update

--- CoreDataProject/DataController.swift
+++ CoreDataProject/DataController.swift
@@ -12,15 +12,18 @@ import Foundation
 class DataController: ObservableObject {
     // Property which is the Core Data type responsible for loading a data model and giving us access to the data inside
     let container = NSPersistentContainer(name: "CoreDataProject")
-    
+
     // Initializer to load our data
     init() {
-        // Tells Core Data to access our saved data according to the data model in Bookworm.xcdatamodeld
+        // If we want Core Data to write changes for duplicate values, we need to adjust the loadPersistentStores() completion handler to specify how data should be merged in this situation
         container.loadPersistentStores { description, error in
-            // If there's an error, print an error message
             if let error = error {
                 print("Core Data failed to load: \(error.localizedDescription)")
+                return
             }
+
+            // This asks Core Data to merge duplicate objects based on their properties - it tries to intelligently overwrite the version in its database using properties from the new version. In our case, because each Wizard has a unique name (which we set up by adding a constraint to our entity), it will try to overwrite the version in the database with the new version.
+            self.container.viewContext.mergePolicy = NSMergePolicy.mergeByPropertyObjectTrump
         }
     }
 }
